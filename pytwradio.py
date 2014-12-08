0ef6766f203b6e0d3332c0057765738dc1e9955d
KiwiKuo <kiwikuo@kkbox.com>
2014-12-08 18:55:03 +0800

    Change retry to wrap function.

--- pytwradio.py
+++ pytwradio.py
@@ -10,18 +10,54 @@ import urllib2
 import re
 import subprocess as sp
 import socket
+import time
+from functools import wraps
 
-def urlopen_with_retry(req, ntries=5):
-    for _ in range(ntries):
-        try:
-            page = urllib2.urlopen(req)
-            break # success
-        except urllib2.URLError as err:
-            if not isinstance(err.reason, socket.timeout):
-               raise # propagate non-timeout errors
-    else: # all ntries failed 
-        raise err # re-raise the last timeout error
-    return page 
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+@retry(urllib2.URLError, tries=5, delay=0, backoff=0)
+def urlopen_with_retry(req):
+    return urllib2.urlopen(req)
 
 class Pytwradio(object):
     """
